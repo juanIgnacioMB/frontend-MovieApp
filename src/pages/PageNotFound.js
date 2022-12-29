e80badd6515aa1a41b00dd24114f05391df24c85
juanIgnacio <juan.melgarejobrabo@gmail.com>
2022-12-29 20:22:26 -0300

    basename

---
+++ src/pages/PageNotFound.js
@@ -0,0 +1,9 @@
+import NavBar from "../components/NavBar"
+export const PageNotFound=()=>{
+    return(
+    <div className="notFoundContainer">
+        <NavBar/>
+        <h1>404 Page not found</h1>
+        </div>
+    )
+} 
